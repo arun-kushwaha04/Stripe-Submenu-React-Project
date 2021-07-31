46273d78f13868ca3a83a6d71bf246c375ff4b2f
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-31 12:26:02 +0530

    NavBar and Hero Component Created

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,34 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobal } from "./context";
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+  const { openSideBar, openSubMenu, closeSubMenu } = useGlobal();
+  return (
+    <nav className="nav">
+      <section className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="stripe-logo" className="nav-logo" />
+          <button className="btn toggle-btn" onClick={openSideBar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">products</button>
+          </li>
+          <li>
+            <button className="link-btn">developers</button>
+          </li>
+          <li>
+            <button className="link-btn">company</button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign in</button>
+      </section>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
