9f2c79b5b32473a81fdc84083ea2627d5fdbb187
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-31 12:07:56 +0530

    Context API created

--- src/context.js
+++ src/context.js
@@ -1,2 +1,42 @@
 import React, { useState, useContext } from "react";
 import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  //defing different state values
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubMenuOpen] = useState(true);
+
+  const openSideBar = () => {
+    setIsSideBarOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSideBarOpen(false);
+  };
+
+  const openSubMenu = () => {
+    setIsSubMenuOpen(true);
+  };
+  const closeSubMenu = () => {
+    setIsSubMenuOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSideBarOpen,
+        isSubmenuOpen,
+        openSideBar,
+        closeSideBar,
+        openSubMenu,
+        closeSubMenu,
+      }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobal = () => {
+  return useContext(AppContext);
+};
+export { AppProvider };
