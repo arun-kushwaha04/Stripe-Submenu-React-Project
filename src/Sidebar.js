fecc6a4c1523f42b4a62cd72aeb26b0fa341f41b
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-31 13:22:54 +0530

    Sidebar Component created

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,42 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import sublinks from "./data";
+import { useGlobal } from "./context";
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  const { isSideBarOpen, closeSideBar } = useGlobal();
+  return (
+    <section
+      className={`${
+        isSideBarOpen ? "sidebar-wrapper show" : "sidebar-wrapper"
+      }`}>
+      <div className="sidebar">
+        <button className="close-btn" onClick={closeSideBar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((title, index) => {
+            return (
+              <article key={index}>
+                <h4>{title.page}</h4>
+                <div className="sidebar-sublinks">
+                  {title.links.map((links, index) => {
+                    const { label, icon, url } = links;
+                    return (
+                      <a href={url} key={index}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
