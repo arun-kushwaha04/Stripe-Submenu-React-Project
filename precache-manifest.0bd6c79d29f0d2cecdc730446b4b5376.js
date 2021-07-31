8a29d0cc98d39c966e2c847fdb46fcb83240ac0c
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-31 14:53:57 +0530

    Updates

---
+++ precache-manifest.0bd6c79d29f0d2cecdc730446b4b5376.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d01fdbf7cc68a387167021b5908d47",
+    "url": "/Stripe-Submenu-React-Project/index.html"
+  },
+  {
+    "revision": "aab6080f515ea2c5aa2a",
+    "url": "/Stripe-Submenu-React-Project/static/css/main.0e2504d3.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/Stripe-Submenu-React-Project/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Stripe-Submenu-React-Project/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aab6080f515ea2c5aa2a",
+    "url": "/Stripe-Submenu-React-Project/static/js/main.e3cd1891.chunk.js"
+  },
+  {
+    "revision": "431498f8e6aef8df8b6c",
+    "url": "/Stripe-Submenu-React-Project/static/js/runtime-main.bebd9964.js"
+  },
+  {
+    "revision": "882a211ad0bae36b9c11715c9291f36b",
+    "url": "/Stripe-Submenu-React-Project/static/media/hero.882a211a.svg"
+  },
+  {
+    "revision": "86d618b5be1508bcbbb31d6393f089f6",
+    "url": "/Stripe-Submenu-React-Project/static/media/logo.86d618b5.svg"
+  },
+  {
+    "revision": "e908706c41907a9611ba9c4d61a32f7c",
+    "url": "/Stripe-Submenu-React-Project/static/media/phone.e908706c.svg"
+  }
+]);
